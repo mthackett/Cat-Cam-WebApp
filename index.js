040a45237817db389cf1d7612ae005651c0e0c65
Matthew Hackett <m4tth@live.com>
2020-08-19 22:39:09 -0700

    Fleshed out Label class. Fixed image caching. Added save labels functionality.

--- index.js
+++ index.js
@@ -3,52 +3,64 @@ var img = new Image();
 
 img.addEventListener('load', loadListener, false);
 
-var previmg = null;
-var nextimg = null;
+var prevImg = null;
+var nextImg = null;
 
 class Label {
-    
+    // Training label class defining a box with a classification label.
     static nextLabelId = 0;
 
-    constructor(canvas, xi,yi) {
-        this.xi = this.xf = xi;
-        this.yi = this.yf = yi;
+    constructor(canvas, imgidx, xi,yi) {
         this.id = Label.nextLabelId++;
+        this.labelIndex = imgidx;
 
-        this.labelinput = $('<input type="text" />');
-        this.labelinput.css('position','absolute');
-        this.labelinput.css('left',this.xi);
-        this.labelinput.css('top',this.yi - 40);
-        $(canvas.parentElement).append(this.labelinput)
+        this.xi = this.xf = this.midx = xi;
+        this.yi = this.yf = this.midy = yi;
+        this.width = this.height = 0;
+
+        this.labelInput = $('<input type="text" />');
+        this.labelInput.css('position','absolute');
+        this.labelInput.css('left',this.xi);
+        this.labelInput.css('top',this.yi - 40);
+        $(canvas.parentElement).append(this.labelInput)
     }
 
     draw(ctx) {
         ctx.beginPath();
-        var width = this.xf-this.xi;
-        var height = this.yf-this.yi;
-        ctx.rect(this.xi,this.yi,width,height);
+        this.calculateCoords();
+        ctx.rect(this.xi,this.yi,this.width,this.height);
         ctx.strokeStyle = 'black';
         ctx.lineWidth = 4;
         ctx.stroke();
     }
 
+    calculateCoords() {
+        this.width = Math.abs(this.xf-this.xi);
+        this.height = Math.abs(this.yf-this.yi);
+
+        this.labelWidth = this.width / img.width;
+        this.labelHeight = this.height / img.height;
+        this.labelX = (this.xi + this.xf) / 2;
+        this.labelY = (this.yi + this.yf) / 2;
+    }
 }
 
 //Canvas
 var canvas = document.getElementById('main-canvas');
 var ctx = canvas.getContext('2d');
 //Labels
-var labels = {};
-currlabel = null;
+var labels = [];
+var currLabel = null;
+var classLookup = {};
 //Variables
-var canvasx = $(canvas).offset().left;
-var canvasy = $(canvas).offset().top;
-var editbox = false;
+var canvasX = $(canvas).offset().left;
+var canvasY = $(canvas).offset().top;
+var editBox = false;
 //Indices
-var previdx = 0;
-var curridx = 0;
-var nextidx = 0;
-var imgkey = 0;
+var prevIdx = 0;
+var currIdx = 0;
+var nextIdx = 0;
+var imgKey = 0;
 
 function loadListener()
 {
@@ -63,70 +75,81 @@ function updateImage(idx, useCached=false)
 {
     $.getJSON(apiBaseUrl + '?getkey&idx=' + idx, function(response)
     {
-        imgkey = response['currkey'];
-        previdx = response['previdx'];
-        nextidx = response['nextidx'];
+        imgKey = response['currkey'];
+        prevIdx = response['previdx'];
+        nextIdx = response['nextidx'];
 
         if (!useCached) {
-            img.src = apiBaseUrl + '?getimg&key=' + imgkey;
+            img.src = apiBaseUrl + '?getimg&key=' + imgKey;
         }
     });
     
 }
 
+function getClasses() 
+{
+
+    $.get(apiBaseUrl + '?getclasses', function(response) {
+            for (keyValPair of JSON.parse(response)) {
+                classLookup[keyValPair['class']] = keyValPair['index'];
+            }
+
+    });
+}
+
 function deleteAllLabels() {
 
-    for (id in labels) {
-        // delete labels[id]
-        labels[id].labelinput.remove();
+    for (label of labels) {
+ 
+        label.labelInput.remove();
     }
-    labels = {};
+    labels = [];
     ctx.drawImage(img,0,0);
 }
 
 function nextImage() 
 {
-    previmg = img.cloneNode();
-    curridx = nextidx;
-    var useCached = (nextimg != null);
+    prevImg = img.cloneNode();
+    currIdx = nextIdx;
+    var useCached = (nextImg != null);
     if  (useCached) {
         deleteAllLabels();
 
-        img = nextimg;
+        img = nextImg;
         img.addEventListener('load', loadListener, false);
         ctx.drawImage(img,0,0);   
     }
     
-    updateImage(curridx, useCached);
+    updateImage(currIdx, useCached);
     
-	nextimg = null;
+	nextImg = null;
 }
 
 function prevImage() 
 {
-    nextimg = img.cloneNode();
-    curridx = previdx;
-    var useCached = (previmg != null);
+    nextImg = img.cloneNode();
+    currIdx = prevIdx;
+    var useCached = (prevImg != null);
     if (useCached) {
         deleteAllLabels();
 
-        img = previmg;
+        img = prevImg;
         img.addEventListener('load', loadListener, false);
         ctx.drawImage(img,0,0); 
     }       
-    updateImage(curridx, useCached);
-	previmg = null;
+    updateImage(currIdx, useCached);
+	prevImg = null;
 }
 
 function deleteImage() 
 {
-// tell lambda to delete img
-    
-    $.get(apiBaseUrl + '?delete&key=' + imgkey, function()
-    { 
-        nextImage();
-    });
-	
+    // tell lambda to delete img
+    $.get(apiBaseUrl + '?delete&key=' + imgKey, nextImage);
+}
+
+function saveLabels() 
+{
+    $.post(apiBaseUrl + '?savelabels&idx=' + currIdx, JSON.stringify(labels, ['labelText','labelX','labelY','normalizedWidth','normalizedHeight']), nextImage, 'json');
 }
 
 function init() 
@@ -134,45 +157,45 @@ function init()
 
 	$('#next-button').click(nextImage);
 	$('#prev-button').click(prevImage);
-	$('#delete-button').click(deleteImage);
-	updateImage(curridx);
+    $('#delete-button').click(deleteImage);
+    $('#save-button').click(saveLabels);
+	updateImage(currIdx);
 }
 
 $(init);
 
 
-
-
 //Mousedown
 $(canvas).on('mousedown', function(e) 
 {
-    x = parseInt(e.clientX-canvasx);
-    y = parseInt(e.clientY-canvasy);
-    if (editbox == false) {
-        currlabel = new Label(canvas, x, y);
-        labels[currlabel.id] = currlabel;
+    x = parseInt(e.clientX-canvasX);
+    y = parseInt(e.clientY-canvasY);
+    if (editBox == false) {
+        currLabel = new Label(canvas, currIdx, x, y);
+        labels.push(currLabel);
 
     }
     else {
         //give label xf and yf.
-        // labels[]
-        currlabel.xf = x;
-        currlabel.yf = y;
+        currLabel.xf = x;
+        currLabel.yf = y;
+        currLabel.calculateCoords();
+        currLabel.labelText = currLabel.labelInput.val();
     }
-    editbox = !editbox;
+    editBox = !editBox;
 });
 
 //Mousemove
 $(canvas).on('mousemove', function(e) 
 {
-    if(editbox) 
+    if(editBox) 
     {
-        currlabel.xf = parseInt(e.clientX-canvasx);
-        currlabel.yf = parseInt(e.clientY-canvasy);
+        currLabel.xf = parseInt(e.clientX-canvasX);
+        currLabel.yf = parseInt(e.clientY-canvasY);
         ctx.clearRect(0,0,canvas.width,canvas.height); //clear canvas
         ctx.drawImage(img,0,0);
-        for (id in labels) {
-            labels[id].draw(ctx);
+        for (label of labels) {
+            label.draw(ctx);
         }
         
     }
